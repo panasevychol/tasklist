fa6eeba83e5ed1067eec78d391dc0cf924f727c2
Oleg Panasevych <panasevychol@gmail.com>
2019-04-30 19:13:32 +0300

    API url initialization changed for a case of multiple apps

--- back/tasklist/api.py
+++ back/tasklist/api.py
@@ -2,10 +2,10 @@ from flask_restful import Api
 
 from tasklist import app
 
-from tasklist.tasks.urls import urls as resources
+from tasklist.tasks.urls import urls as tasks_urls
 
 
 api = Api(app)
 
-for resource in resources:
+for resource in [*tasks_urls, ]:
     api.add_resource(*resource)
